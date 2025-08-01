6c7d82d0923bdc8aada7b09271224cadba9a9ceb
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2025-08-01 12:26:37 -0300

    Initial Commit

---
+++ MannyCrypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MannyCrypto
+//
+//  Created by Manuel Alvarez on 01/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MannyCrypto/MannyCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MannyCryptoApp.swift
+//  MannyCrypto
+//
+//  Created by Manuel Alvarez on 01/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MannyCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
