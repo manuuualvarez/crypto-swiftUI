34b032289f1662a0bbc5da578f1753a8ffdd0194
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2025-08-01 19:23:57 -0300

    Add CoinRowView, CoinModel, and Double extensions for currency formatting
    - Introduced CoinRowView for displaying cryptocurrency data in a row format.
    - Added CoinModel to represent cryptocurrency data with properties for price, market cap, and more.
    - Implemented Double extensions for currency formatting and number string conversions.
    - Included PreviewProvider for easier UI previews in different themes.

---
+++ MannyCrypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinRowView.swift
+//  MannyCrypto
+//
+//  Created by Manuel Alvarez on 01/08/2025.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinRowView()
+}
