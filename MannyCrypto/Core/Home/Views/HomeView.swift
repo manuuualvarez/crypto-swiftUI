3c591c9ee458bf53bac40c5984dd38d332bf0e11
Manuel Alvarez <alvarezmanuelignacio@gmail.com>
2025-08-02 22:15:00 -0300

    Refactor CoinDataServices to utilize NetworkingManager for API requests and streamline HomeView layout
    - Replaced direct URLSession calls in CoinDataServices with a new NetworkingManager for better error handling and code organization.
    - Removed unnecessary whitespace in HomeView for cleaner layout.

--- MannyCrypto/Core/Home/Views/HomeView.swift
+++ MannyCrypto/Core/Home/Views/HomeView.swift
@@ -17,7 +17,6 @@ struct HomeView: View {
             
             VStack {
                 homeHeader
-                
                 columTitles
 
                 if !showPortfolio {
--- MannyCrypto/Services/CoinDataServices.swift
+++ MannyCrypto/Services/CoinDataServices.swift
@@ -19,28 +19,11 @@ class CoinDataServices {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else {
             return
         }
-        coinSuscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                response.statusCode >= 200 && response.statusCode < 300
-                else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSuscription = NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { (completion) in
-                switch completion {
-                case .failure(let error):
-                    print("Error: \(error.localizedDescription)")
-                case .finished:
-                    break
-                }
-            } receiveValue: { [weak self] (returnedCoins) in
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
                 self?.coinSuscription?.cancel()
-            }
+            })
     }
 }
